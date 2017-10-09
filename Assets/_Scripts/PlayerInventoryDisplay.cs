48e9905614c33eece988d4c1ece3820ed98c1467
dr-matt-smith <dr_matt_smith@me.com>
2017-10-09 08:15:00 +0100

    added code for project

---
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerInventory))]
+public class PlayerInventoryDisplay : MonoBehaviour 
+{
+	const int NUM_INVENTORY_SLOTS = 10;
+
+	// array of references to slot panel GameObjects
+	private PickupUI[] slots = new PickupUI[NUM_INVENTORY_SLOTS];
+
+	public GameObject panelSlotGrid;
+	public GameObject starSlotPrefab;
+
+	void Awake()
+	{
+		float width = 50 + (NUM_INVENTORY_SLOTS * 50);
+
+		panelSlotGrid.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+
+		for(int i=0; i < NUM_INVENTORY_SLOTS; i++){
+			GameObject starSlotGO = (GameObject)
+				Instantiate(starSlotPrefab);
+			starSlotGO.transform.SetParent(panelSlotGrid.transform);
+			starSlotGO.transform.localScale = new Vector3(1,1,1);
+			slots[i] = starSlotGO.GetComponent<PickupUI>();
+		}
+	}
+
+	
+	public void OnChangeStarTotal(int starTotal)
+	{
+		for(int i = 0; i < NUM_INVENTORY_SLOTS; i++){
+			PickupUI slot = slots[i];
+			if(i < starTotal)
+				slot.DisplayColorIcon();
+			else
+				slot.DisplayGreyIcon();
+		}
+	}
+}
